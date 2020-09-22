df0ec642c3d0d4314623de7e53fccddbdc81f1ce
ALEJANDRO JEMIC AT HOME <alejandro.jemic@tecnova.cl>
2020-09-22 01:07:27 -0300

    add running time, add reset

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Commands } from '../models/bitmex.model';
-import{BitmexService} from '../services/bitmex.service'
+import{BitmexService} from '../services/bitmex.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import{BitmexService} from '../services/bitmex.service'
 })
 export class HomePage {
   startTime: Date ;
+  runingTime: string;
   bidPrice : number = 0;
   delay: string;
   max: number = 0;
@@ -51,11 +53,32 @@ export class HomePage {
       this.maxPercent = +((this.maxDelta * 100) / this.bidPrice).toFixed(2);
       this.totalDelta + this.minDelta + this.maxDelta;
       this.totalPercent = this.maxPercent + this.minPercent;
+      this.runingTime = this.elapsedTime(localTime.getTime() - this.startTime.getTime());
     });
   }
 
 
+  elapsedTime (dif){
+    var du = moment.duration(dif, "milliseconds");
+    var d =  Math.round(du.days() +( du.months() * 30) + (du.years() *365 )).toString(); 
+    var h =  String("000" + Math.round(du.hours()).toString()).slice(-2);
+    var m =  String("000" + Math.round(du.minutes()).toString()).slice(-2);
+    var s =  String("000" + Math.round(du.seconds()).toString()).slice(-2);
+    return d + 'd ' + h + ':' + m + ':' + s
+  }
 
+  onReset(){
+    this.startTime = new Date();
+    this.max =this.bidPrice;
+    this.min =this.bidPrice;
+    this.maxDelta = 0;
+    this.minDelta = 0;
+    this.minPercent =  +(0).toFixed(2);
+    this.maxPercent = +(0).toFixed(2);
+    this.totalDelta +0;
+    this.totalPercent = 0;
+    this.runingTime = this.elapsedTime(new Date().getTime() - this.startTime.getTime());
+  }
 
 }
 
